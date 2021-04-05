6608e6120640710fe75eba53c5f3c1ccc266f544
Sergey <sgolubnik@gmail.com>
2021-04-05 20:56:06 +0300

    Initial Commit

---
+++ Place-To-Place/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Place-To-Place
+//
+//  Created by СОВА on 05.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
